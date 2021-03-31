6107dc0b6fd4136ef3a7d80b88d8a9115fb7dc72
唐兵 <tangbing@kingchannels.com>
2021-03-31 18:57:16 +0800

    添加Rsa加解密

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.cipher.AESUtil
+import com.example.myapplication.cipher.RSAUtils
 import com.example.myapplication.coroutine.CoroutineTestActivity
 import com.example.myapplication.touchevent.DispatchEventActivity
 import com.example.myapplication.touchevent.NestedScrollTestActivity
@@ -28,7 +30,8 @@ class MainActivity : AppCompatActivity() {
         var kotlinCoroutineTest:TextView=findViewById(R.id.kotlin_coroutine)
         var nestedScrollTest:TextView=findViewById(R.id.nestedScrollTest)
         var dispatchEvent:TextView=findViewById(R.id.dispatchEvent)
-        var cipher:TextView=findViewById(R.id.cipher)
+        var cipher:TextView=findViewById(R.id.cipherAES)
+        var cipherRSA:TextView=findViewById(R.id.cipherRSA)
 
         overWriteView?.setOnClickListener {
             CommonActivity.startActivity(this, ViewType.WriteView)
@@ -53,6 +56,11 @@ class MainActivity : AppCompatActivity() {
 //                AESUtil.decryptFile( File("file:///android_asset/1.xhtml"),"01.xhtml","file:///android_asset/","qDwnHTTcq18X7lGD");
                 AESUtil.decryptFile( assets.open("1.xhtml"),"01.xhtml","/storage/emulated/0/","qDwnHTTcq18X7lGD");
             }
+        }
+        cipherRSA?.setOnClickListener{
+            var hashMap=RSAUtils.initKey()
+            Log.d("RSA","PUBLIC_KEY------"+ RSAUtils.getPublicKey(hashMap));
+            Log.d("RSA","PRIVATE_KEY------"+ RSAUtils.getPrivateKey(hashMap));
         }
     }
 
